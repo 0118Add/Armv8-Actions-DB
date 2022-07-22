244b6f01897f2af94727a1e76a921bbe0769f8a6
0118Add <60157610+0118Add@users.noreply.github.com>
2022-07-22 20:25:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -83,7 +83,7 @@ git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/8688Add/luci-app-vssr.git package/luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 #git clone https://github.com/bin20088/luci-app-koolddns.git package/luci-app-koolddns
 #git clone https://github.com/QiuSimons/openwrt-mos package/luci-app-mosdns
