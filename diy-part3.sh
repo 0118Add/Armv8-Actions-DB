b29773109ae5e242664f642995927096da835810
0118Add <60157610+0118Add@users.noreply.github.com>
2022-08-17 14:51:16 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -4,6 +4,9 @@ sed -i "s/hostname='.*'/hostname='N1'/g" package/base-files/files/bin/config_gen
 # 设置密码为空
 # sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
 
+#设置ttyd免帐号登录
+sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/ttyd.config
+
 git clone https://github.com/gd0772/package package/gd772
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial_new package/luci-theme-atmaterial_new
