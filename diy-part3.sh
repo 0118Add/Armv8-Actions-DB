a5a507dfd3fa23e0883c3e58dc21c2e638c3babd
0118Add <60157610+0118Add@users.noreply.github.com>
2022-08-17 15:14:21 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -8,6 +8,7 @@ sed -i "s/hostname='.*'/hostname='N1'/g" package/base-files/files/bin/config_gen
 sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/ttyd.config
 
 git clone https://github.com/gd0772/package package/gd772
+rm -rf package/gd772/theme/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial_new package/luci-theme-atmaterial_new
 wget https://raw.githubusercontent.com/0118Add/patch/main/n1.sh
