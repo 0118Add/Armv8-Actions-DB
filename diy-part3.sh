f9a374de73bdf7996aed97d405d59719bb68189c
0118Add <60157610+0118Add@users.noreply.github.com>
2022-10-03 22:16:23 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -36,11 +36,18 @@ git clone https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial_new package/luci-theme-atmaterial_new
 git clone https://github.com/ophub/luci-app-amlogic.git package/amlogic
+git clone https://github.com/sbwml/luci-app-alist.git package/alist
+sed -i 's/Alist 文件列表/文件列表/g' package/luci-app-alist/luci-app-alist/po/zh-cn/alist.po
 wget https://raw.githubusercontent.com/0118Add/patch/main/n1.sh
 bash n1.sh
 
 # 修改 bypass 依赖
 sed -i 's/luci-lib-ipkg/luci-base/g' package/luci-app-bypass/Makefile
+
+# 调整 Alist 文件列表 到 系统 菜单
+sed -i 's/NAS/d' package/alist/luci-app-alist/luasrc/controller/*.lua
+sed -i 's/nas/system/g' package/alist/luci-app-alist/luasrc/controller/*.lua
+sed -i 's/nas/system/g' package/alist/luci-app-alist/luasrc/view/alist/*.htm
 
 # 晶晨宝盒
 sed -i "s|https.*/s9xxx-openwrt|https://github.com/0118Add/N1dabao|g" package/amlogic/luci-app-amlogic/root/etc/config/amlogic
