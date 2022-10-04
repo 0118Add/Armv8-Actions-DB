c80376f433f2646746f9eca5dd98323a00a4bfc5
0118Add <60157610+0118Add@users.noreply.github.com>
2022-10-04 13:07:42 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -40,7 +40,7 @@ git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial_new package/luci-theme-atmaterial_new
 git clone https://github.com/ophub/luci-app-amlogic.git package/amlogic
 git clone https://github.com/sbwml/luci-app-alist.git package/alist
-sed -i 's/Alist 文件列表/文件列表/g' package/luci-app-alist/luci-app-alist/po/zh-cn/alist.po
+sed -i 's/Alist 文件列表/文件列表/g' package/alist/luci-app-alist/po/zh-cn/alist.po
 wget https://raw.githubusercontent.com/0118Add/patch/main/n1.sh
 bash n1.sh
 
